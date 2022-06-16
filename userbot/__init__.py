2f3290bb99c89c97758bed3d204294b310a845e0
BTRExo <101071339+BTRExo@users.noreply.github.com>
2022-06-17 00:57:13 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -656,7 +656,7 @@ with bot:
             if event.query.user_id == uid or event.query.user_id in DEVS and SUDO_USERS:
                 openlagi = custom.Button.inline("• Re-Open Menu •", data="reopen")
                 await event.edit(
-                    "🔒 **Help Mode Button Ditutup!** 🔒", buttons=openlagi
+                    "🔒 **Help Mode Button Ditutup!** ", buttons=openlagi
                 )
             else:
                 reply_pop_up_alert = f"Kamu Tidak diizinkan, ini Userbot Milik {owner}"
